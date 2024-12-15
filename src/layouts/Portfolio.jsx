c6c0c8e6d75818fc03ce829647a2607a7f4926c1
Dahlberg94 <Marcus_dahlberg_94@hotmail.com>
2024-12-15 16:45:25 +0100

    fixade fler project

--- src/layouts/Portfolio.jsx
+++ src/layouts/Portfolio.jsx
@@ -1,6 +1,8 @@
 import { NavLink, Outlet } from 'react-router-dom'
 import '../index.css'
-import pichtmlcss from '../Assets/HTML-CSS.jpg'
+import pichtmlcss from '../Assets/portfoliopic/HTML-CSS.jpg'
+import picpwgen from '../Assets/portfoliopic/pwgen.jpg'
+import picmotivation from '../Assets/portfoliopic/motivation.jpg'
 
 
 const Portfolio = () => {
@@ -11,9 +13,16 @@ const Portfolio = () => {
             <p>Här är min samling av projekt och arbeten.</p>
             </div>
             <div className='project'>
-                <a href="https://order-summary-md.vercel.app/" >
-                <img src={pichtmlcss} alt=""  /></a>
+               <div><a href="https://order-summary-md.vercel.app/" >
+               <img src={pichtmlcss} alt=""  /></a></div> 
+               <div><a href="https://pw-gen-md.vercel.app/" >
+               <img src={picpwgen} alt=""  /></a></div> 
+               <div><a href="https://motivation-md.vercel.app/" >
+               <img src={picmotivation} alt=""  /></a></div> 
+                
+           
             </div>
+            
         </div>
     );
 };
